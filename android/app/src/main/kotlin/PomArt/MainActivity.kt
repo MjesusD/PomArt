28d690373dd6d8660517547b7e4d46f447bd879b
mbpaw <mbpaw7@gmail.com>
2025-06-18 16:15:35 -0400

    Laboratorio 10

--- android/app/src/main/kotlin/PomArt/MainActivity.kt
+++ android/app/src/main/kotlin/PomArt/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pomart
+package PomArt
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:pomart/pages/splash.dart';
-
+import 'package:pomart/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final materialTheme = MaterialTheme(ThemeData.light().textTheme); 
+
     return MaterialApp(
-      title: 'POMART APP',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme( backgroundColor: Color.fromARGB(255, 212, 173, 235)),
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 187, 128, 221)),
-      ),
+      theme: materialTheme.light(),         // Tema claro
+      //darkTheme: materialTheme.dark(),   // Tema oscuro
+      //themeMode: ThemeMode.system,      // Sistema operativo decide
       home: const SplashPage(),
     );
   }
 }
-
